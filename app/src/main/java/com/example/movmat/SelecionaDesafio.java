356bbae757540a14faeeed500b61e214ce4dc74d
unknown <rafael.bds25@gmail.com>
2020-03-21 16:43:23 -0300

    criando tela de desafio

--- app/src/main/java/com/example/movmat/SelecionaDesafio.java
+++ app/src/main/java/com/example/movmat/SelecionaDesafio.java
@@ -2,20 +2,37 @@ package com.example.movmat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class SelecionaDesafio extends AppCompatActivity {
     public Aluno aluno = new Aluno();
-
+    public TextView botaoSoma;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleciona_desafio);
+
+        //pegando botões
+        botaoSoma = (TextView) findViewById(R.id.botaoSoma);
+
+        //recebendo dados
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
             aluno.setNomeAluno(extras.getString("nomeAluno"));//inserindo dados
             Toast.makeText(SelecionaDesafio.this, ""+ aluno.getNomeAluno(), Toast.LENGTH_SHORT).show();
         }
+        //enviando dados
+        botaoSoma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SelecionaDesafio.this, TelaDesafio.class);
+                intent.putExtra("nomeAluno", aluno.getNomeAluno());
+                startActivity(intent);
+            }
+        });
     }
 }
